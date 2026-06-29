03e451ea4a651090638422f818b4dddef10f53ef
Tefuko <sakura.math.sashimo@gmail.com>
2026-06-30 00:16:34 +0900

    テーマカラー変更

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,8 +7,8 @@ export default function manifest(): MetadataRoute.Manifest {
     description: '水耕栽培の成長記録',
     start_url: '/',
     display: 'standalone',
-    background_color: '#f2f8d7',
-    theme_color: '#419457',
+    background_color: '#fdfff0',
+    theme_color: '#376d44',
     icons: [
       { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
       { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
